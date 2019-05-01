702605c54931e0cfabfa875c99d1c876321c120b
Angelo Bovino <angelo.bovino@outlook.com>
2019-04-30 21:33:49 -0400

    Use short_description instead of API description because API descriptions are weird

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -4,16 +4,25 @@ import './Weather.css';
 import WeatherIcon from './WeatherIcon/WeatherIcon.js';
 
 const Weather = ( props ) => {
-	console.log(props.weather.weather[0].description);
-	let description = props.weather.weather[0].description;
-	let temperature = (props.weather.main.temp - 273.15) * 9 / 5 + 32;
-	let city = props.weather.name;
+	const currentWeather = props.weather.weather[0];
+	const temperature = (props.weather.main.temp - 273.15) * 9 / 5 + 32; // convert kelvin to fahrenheit
+	const city = props.weather.name;
+	let description = '';
+
+	props.weatherConditions.map( condition => {
+		if (currentWeather.id === condition.id) {
+			description = condition.short_description;
+		}
+	})
+
 	return (
 		<div key={props.datetime} className="weather-container">
 			<p className="city">{city}</p>
 			<p className="description">{description}</p>
 			<p className="temperature">{temperature.toFixed(0)}&#176;</p>
-			<WeatherIcon weather={props.weather.weather[0]}/>
+			<WeatherIcon 
+				weather={props.weather.weather[0]}
+				weatherConditions={props.weatherConditions} />
 		</div>
 	)
 }

71a9a7967bb1207939a1142f70f969e72cb5613c
Angelo Bovino <angelo.bovino@outlook.com>
2019-05-01 20:17:58 -0400

    Add 24 hour weather scroll container

--- src/components/Weather/HourlyWeather/HourlyWeather.js
+++ src/components/Weather/HourlyWeather/HourlyWeather.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
+import './HourlyWeather.css';
+
 const HourlyWeather = ( props ) => {
 	let hours = props.hourlyWeather.list.map( (hour, index) => {
-		if (index < 12) {
+		if (index < 24) {
+			const time = new Date(hour.dt * 1000).toLocaleString(undefined, { hour: '2-digit' }).toLowerCase();
+			let iconClasses = '';
+
+			for (let i = 0; i < props.weatherConditions.length; i++) {
+				if (hour.weather[0].id === props.weatherConditions[i].id) {
+					iconClasses = props.weatherConditions[i].day_icon_class;
+				}
+			}
+
 			return (
-				<section>
-					<span>{((hour.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)}</span>
-					<span>{hour.weather[0].main}</span>
-				</section>
+				<tr key={hour.dt} className="hour">
+					<td>{time}</td>
+					<td>{((hour.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)}&#176;</td>
+					<td><i className={`wi ${iconClasses}`}></i></td>
+				</tr>
 			)
 		}
 	})
 	return (
-		<div>
-			{hours}
+		<div className="hourly-container">
+			<table className="hourly-table">
+				<tbody>
+					{hours}
+				</tbody>	
+			</table>
 		</div>
 	)
 }

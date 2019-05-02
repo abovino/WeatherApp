2302236210cf5093ee704091005bdba39679fb0a
Angelo Bovino <angelo.bovino@outlook.com>
2019-05-01 21:47:14 -0400

    Replace .map function with for loop because no value is returned, only show next 24 hrs of data, and show day or night icons

--- src/components/Weather/HourlyWeather/HourlyWeather.js
+++ src/components/Weather/HourlyWeather/HourlyWeather.js
@@ -3,14 +3,22 @@ import React from 'react';
 import './HourlyWeather.css';
 
 const HourlyWeather = ( props ) => {
+	const weatherConditions = props.weatherConditions;
+
 	let hours = props.hourlyWeather.list.map( (hour, index) => {
-		if (index < 24) {
+		// Include only the next 24 hours
+		if (index < 8) {
 			const time = new Date(hour.dt * 1000).toLocaleString(undefined, { hour: '2-digit' }).toLowerCase();
 			let iconClasses = '';
 
-			for (let i = 0; i < props.weatherConditions.length; i++) {
-				if (hour.weather[0].id === props.weatherConditions[i].id) {
-					iconClasses = props.weatherConditions[i].day_icon_class;
+			for (let i = 0; i < weatherConditions.length; i++) {
+				if (hour.weather[0].id === weatherConditions[i].id) {
+					if (props.isLightOut) {
+						iconClasses = weatherConditions[i].day_icon_class;
+						break;
+					}
+					iconClasses = weatherConditions[i].night_icon_class;
+					break;
 				}
 			}
 

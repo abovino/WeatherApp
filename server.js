88c86a351c8b0c946fb328ec85cd3b92e774ae0c
Angelo Bovino <angelo.bovino@outlook.com>
2019-05-11 09:04:21 -0400

    Change static files directory to 'public'

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ip = require('ip');
 const PORT = 3001;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.sendFile('index.html');
 });
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${ip.address()}:${PORT}`));
